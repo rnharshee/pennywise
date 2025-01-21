1159d948fecb850009900e97307ca3ddc2e386d8
Harshita-Raghu <harshita.raghu@nividapps.com>
2025-01-21 17:32:38 +0530

    First commit

---
+++ src/test/java/com/harshee/pennywise/PennywiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshee.pennywise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PennywiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
